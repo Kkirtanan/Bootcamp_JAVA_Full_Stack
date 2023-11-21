b4afa165557c59485a1c9725cd908ee4e448e412
Kkirtanan <kelysr18@gmail.com>
2023-11-21 22:15:41 +0000

    aula_dia_21_11

---
+++ JS/aula-1/script.js
@@ -0,0 +1,58 @@
+
+// console.log("Hello, Brazil.")
+// console.log("Turma JAVA Full Stack")
+
+// Variáveis
+// let e var: pode ser modificada no decorrer do script, let pode ser usado em qualquer parte do sistema, var é mais restrita
+// const: não altera
+// * não pode começar com número
+// * não usar caracteres
+// * não usar palavras reservadas
+
+// Incializar varíavel
+// let nome:
+// let idade:
+
+// Definindo valores
+// nome= "Kely";
+// idade= 30;
+
+// Saída de dados para teste
+// console.log(nome)
+// console.log(idade)
+
+// Variáveis numéricas
+// let num1= 18;
+// let num2= 18;
+// let soma= num1 + num2;
+
+// console.log("A soma de", num1, "+", num2, "é =", soma)
+
+// Variáveis de texto (STRINGS)
+// let saudacao = "Olá";
+// let nome = "Kely";
+// let mensagem = saudacao + " " + nome + "! Bem - vinda.";
+// console.log(mensagem);
+
+// Variáveis de Array: armazena uma lista de valores
+// let frutas = ["maçã", "mamão", "uva", "morango", "laranja", "banana"];
+// console.log("Minha lista de frutas: ",frutas[3]);
+
+// Variáveis de objetos
+// let pessoa ={
+//   nome: "Kely",
+//   idade: 30,
+//   cidade: "São Paulo"
+// }
+// console.log("Dados da pessoa: ", pessoa)
+// console.log(pessoa.nome + " tem " + pessoa.idade + " anos e mora em " + pessoa.cidade)
+
+// Tipos de variáveis
+// * number
+// * String
+// * boolean
+// * null (nulo)
+// * undefined 
+// * array (lista): [1,2,3,4,]
+// * object: {nome:"Kely", idade: 30, }
+
